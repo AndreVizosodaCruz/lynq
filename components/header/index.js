99c502c78d663b7435d095e6e99df95b64620522
Joao Cruz <joaoandredcruz@gmail.com>
2022-01-15 12:56:31 +0000

    backup

--- components/header/index.js
+++ components/header/index.js
@@ -4,7 +4,10 @@ import styled from 'styled-components'
 export default function Header() {
     return (
         <Container>
-
+            <Icon />
+            <Name>
+                Ben Burns
+            </Name>
         </Container>
     )
 }
@@ -14,4 +17,20 @@ const Container = styled.div`
     padding: 50px 0 24px;
     width: 100%;
     box-sizing: border-box;
+`;
+const Icon = styled.div`
+    width: 63px;
+    height: 63px;
+    background: #38383D;
+    border-radius: 20px;
+    margin-bottom: 12px;
+`;
+const Name = styled.div`
+    font-family: 'Inter', sans-serif;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 18px;
+    line-height: 27px;
+    text-align: center;
+    color: #FFFFFF;
 `;
