dafd0ad85724584f4d9c2681553fce9ceb37f4cf
Joao Cruz <joaoandredcruz@gmail.com>
2022-01-15 13:50:30 +0000

    backup

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { Footer, Form, Header } from '../components'
+import { Footer, Form, Header } from '../customComponents'
 
 
 export default function Home() {
