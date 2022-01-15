ba99f90370a659d0f1ad23b1636c4126c6f2576c
Joao Cruz <joaoandredcruz@gmail.com>
2022-01-15 13:43:05 +0000

    backup

--- components/Form/index.js
+++ components/Form/index.js
@@ -1,16 +1,84 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { useState } from 'react'
+import styled, { css } from 'styled-components'
+
+const CustomInput = (props) => {
+
+    return (
+        <Margin margin={props.margin}>
+            <Text size="14px" height="17px" color="#BEC2C9" dangerouslySetInnerHTML={{ __html: props.label }} />
+            <Margin margin={0.5} />
+            <StyledInput type={props.type} placeholder={props.placeholder} value={props.value} onChange={props.onChange} />
+        </Margin>
+    )
+
+}
 
 export default function Form() {
+
+    const [values, setValues] = useState({ firstName: '', lastName: '', email: '' })
     return (
         <Container>
-
+            <ContainerForm>
+                <Text dangerouslySetInnerHTML={{ __html: 'Your info' }} />
+                <Margin margin={2} />
+                <CustomInput label='Name' placeholder='Enter your name' margin={2.5} type="text" value={values.firstName} onChange={(e) => setValues({ ...values, firstName: e.target.value })} />
+                <CustomInput label='Last name' placeholder='Enter your last name' margin={2.5} type="text" value={values.lastName} onChange={(e) => setValues({ ...values, lastName: e.target.value })} />
+                <CustomInput label='Email' placeholder='Enter your email' margin={2.5} type="email" value={values.email} onChange={(e) => setValues({ ...values, email: e.target.value })} />
+            </ContainerForm>
         </Container>
     )
 }
 
+const Margin = styled.div`
+    margin-bottom: ${({ margin }) => margin ? margin * 8 : 8}px;
+`;
 const Container = styled.div`
     flex: 1;
     width: 100%;
     box-sizing: border-box;
+    padding: 24px 0;
+    box-sizing: border-box;
+    overflow-y: auto;
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+`;
+const ContainerForm = styled.div`
+    display: flex;
+    flex-flow: column;
+`;
+const Text = styled.div`
+    font-size: ${props => props.size || "18px"};
+    line-height: ${props => props.height || "22px"};
+    color: ${props => props.color || "#FFFFFF"};
+    font-weight: ${props => props.weight || "normal"};
+`;
+const StyledInput = styled.input`
+    width: 345px;
+    height: 50px;
+    background: #393B3F;
+    border: 1px solid #393B3F;
+    outline: none;
+    box-sizing: border-box;
+    border-radius: 5px;
+    padding: 16px;
+    box-sizing: border-box;
+    transition: all 0.4s ease-in-out;
+    ::placeholder{
+        font-size: 16px;
+        line-height: 19px;
+        color: #999CA0;
+    }
+    :focus{
+        background: #FFFFFF;
+        border: 1px solid #4CC9F0;
+        box-sizing: border-box;
+        box-shadow: 0px 0px 20px rgba(76, 201, 240, 0.3);
+    }
+    ${({ value }) => value !== '' && css`
+        background: #BEC2C9;
+        border: 1px solid #393B3F;
+        box-sizing: border-box;
+        border-radius: 5px;
+    `}
 `;
--- components/header/index.js
+++ components/header/index.js
@@ -8,6 +8,11 @@ export default function Header() {
             <Name>
                 Ben Burns
             </Name>
+            <DivNavigation>
+                <Text color='#393B3F' dangerouslySetInnerHTML={{ __html: 'Select date & time' }} />
+                <IconArrow src='./static/img/arrow.svg' />
+                <Text dangerouslySetInnerHTML={{ __html: 'Payment' }} />
+            </DivNavigation>
         </Container>
     )
 }
@@ -17,13 +22,19 @@ const Container = styled.div`
     padding: 50px 0 24px;
     width: 100%;
     box-sizing: border-box;
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    justify-content: center;
 `;
 const Icon = styled.div`
-    width: 63px;
-    height: 63px;
+    width: 70px;
+    height: 70px;
     background: #38383D;
     border-radius: 20px;
     margin-bottom: 12px;
+    border: 3.5px solid #191919;
+    box-sizing: border-box;
 `;
 const Name = styled.div`
     font-family: 'Inter', sans-serif;
@@ -33,4 +44,22 @@ const Name = styled.div`
     line-height: 27px;
     text-align: center;
     color: #FFFFFF;
+    margin-bottom: 24px;
+`;
+const DivNavigation = styled.div`
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+`;
+const Text = styled.div`
+    color: ${props => props.color || "#BEC2C9"};
+    font-family: 'Inter', sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 14px;
+`;
+const IconArrow = styled.img`
+    max-width: 15px;
+    margin: 0 12px;
 `;
